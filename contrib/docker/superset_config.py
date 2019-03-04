d29c162b3c3dbab7ec903421c088d5bcafe13ef8
mohan-kumar <mohan.kumar@enlightiks.com>
2019-03-04 15:20:13 +0530

    superset config update

--- contrib/docker/superset_config.py
+++ contrib/docker/superset_config.py
@@ -1,21 +1,28 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from celery.schedules import crontab
+
 import os
 
+from flask_appbuilder.security.manager import AUTH_OID
+
+APP_NAME = 'Querent'
+
+APP_ICON = '/static/assets/images/practo_logo_blue.png'
+
+ENABLE_PROXY_FIX = False
+
+AUTH_TYPE = AUTH_OID
+
+OPENID_PROVIDERS = [
+    {'name': 'Practo', 'url': ''}]
+
+ENABLE_CORS = False
+
+HTTP_HEADERS = {}
 
 def get_env_variable(var_name, default=None):
     """Get the environment variable or raise exception."""
@@ -49,10 +56,81 @@ REDIS_PORT = get_env_variable('REDIS_PORT')
 
 class CeleryConfig(object):
     BROKER_URL = 'redis://%s:%s/0' % (REDIS_HOST, REDIS_PORT)
-    CELERY_IMPORTS = ('superset.sql_lab', )
+    CELERY_IMPORTS = ('superset.sql_lab', 'superset.tasks')
     CELERY_RESULT_BACKEND = 'redis://%s:%s/1' % (REDIS_HOST, REDIS_PORT)
-    CELERY_ANNOTATIONS = {'tasks.add': {'rate_limit': '10/s'}}
+    CELERYD_LOG_LEVEL = 'DEBUG'
+    CELERYD_PREFETCH_MULTIPLIER = 10
+    CELERY_ACKS_LATE = True
+    CELERY_ANNOTATIONS = {
+        'tasks.add': {
+            'rate_limit': '10/s'
+        },
+        'sql_lab.get_sql_results': {
+            'rate_limit': '100/s',
+        },
+        'email_reports.send': {
+            'rate_limit': '1/s',
+            'time_limit': 120,
+            'soft_time_limit': 150,
+            'ignore_result': True,
+        },
+    }
+    CELERYBEAT_SCHEDULE = {
+        'email_reports.schedule_hourly': {
+            'task': 'email_reports.schedule_hourly',
+            'schedule': crontab(minute=1, hour='*'),
+        },
+    }
     CELERY_TASK_PROTOCOL = 1
 
 
 CELERY_CONFIG = CeleryConfig
+
+CACHE_CONFIG = {
+    'CACHE_TYPE': 'redis',
+    'CACHE_DEFAULT_TIMEOUT': 60 * 60 * 24, # 1 day default (in secs)
+    'CACHE_KEY_PREFIX': 'superset_results',
+    'CACHE_REDIS_URL': 'redis://%s:%s/0' % (REDIS_HOST, REDIS_PORT),
+}
+
+
+MAPBOX_API_KEY = os.environ.get('MAPBOX_API_KEY', '')
+
+ENABLE_SCHEDULED_EMAIL_REPORTS = True
+
+EMAIL_NOTIFICATIONS = True  # all the emails are sent using dryrun
+SMTP_HOST = 'smtp.gmail.com'
+SMTP_STARTTLS = True
+SMTP_SSL = False
+SMTP_USER = ''
+SMTP_PORT = 587
+SMTP_PASSWORD = ''
+SMTP_MAIL_FROM = 'superset@superset.com'
+
+EMAIL_REPORTS_WEBDRIVER = 'firefox'
+
+WEBDRIVER_BASEURL = 'http://0.0.0.0:9099/'
+
+
+# Uncomment below code if running behind a proxy server(nginx) and mapped to
+# prefix url(ex: # xxx.host.com/superset)
+# class ReverseProxied(object):
+#
+#     def __init__(self, app):
+#         self.app = app
+#
+#     def __call__(self, environ, start_response):
+#         script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+#         if script_name:
+#             environ['SCRIPT_NAME'] = script_name
+#             path_info = environ['PATH_INFO']
+#             if path_info.startswith(script_name):
+#                 environ['PATH_INFO'] = path_info[len(script_name):]
+#
+#         scheme = environ.get('HTTP_X_SCHEME', '')
+#         if scheme:
+#             environ['wsgi.url_scheme'] = scheme
+#         return self.app(environ, start_response)
+#
+#
+# ADDITIONAL_MIDDLEWARE = [ReverseProxied, ]
